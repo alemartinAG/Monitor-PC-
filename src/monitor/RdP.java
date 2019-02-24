c2f1ea59678a6ee3a7007cfd5b49347556c8a5e3
alemartinAG <ale.martin96@hotmail.com>
2019-02-24 20:25:05 -0300

    Generador de matriz

--- src/monitor/RdP.java
+++ src/monitor/RdP.java
@@ -4,16 +4,50 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class RdP {
 	
-	private BufferedReader br;
-	private String textfile;
-	private static final String BEG = "Combined incidence matrix"; 
-	private static final String END = "Inhibition matrix";
-	private static final String CELL = "\"cell\">";
+	private Vector<int[]> petriMatrix; 
+	
+
 	
 	public RdP() {
+		
+		petriMatrix = new Vector<int[]>();
+		petriMatrix = getMatrix();
+		
+		System.out.printf("\nCombined incidence matrix:\n\n");
+		
+		for(int i=0; i<petriMatrix.size(); i++) {
+			for(int j=0; j<petriMatrix.get(i).length; j++) {
+				System.out.printf("%2d ", petriMatrix.get(i)[j]);
+			}
+			
+			System.out.println(" ");
+		}
+		
+	}
+	
+	public void sensibilizadas(){
+		
+	}
+	
+	public boolean disparar(){
+		
+		return false;
+	}
+	
+	private Vector<int[]> getMatrix(){
+		
+		BufferedReader br;
+		String textfile = "";
+		
+		final String BEG = "Combined incidence matrix"; 
+		final String END = "Inhibition matrix";
+		final String CELL = "\"cell\">";
+		
+		int columnas = 0;
 		
 		try{
 			
@@ -31,40 +65,62 @@ public class RdP {
 		    textfile = textfile.substring(textfile.indexOf(BEG), textfile.indexOf(END));
 		    textfile = textfile.substring(textfile.indexOf(CELL)+CELL.length());
 		    
-		    //System.out.print(textfile);
-		    
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 		
-		ArrayList<String> matriz = new ArrayList<>();
+		//Cuenta la cantidad de columnas
+		final String KEYWRD = "colhead";
+		String colheads = textfile.substring(0, textfile.indexOf("rowhead"));
 		
 		while(true) {
 			
 			try {
 				
-				textfile = textfile.substring(textfile.indexOf(CELL));
-				matriz.add(textfile.substring(CELL.length(), textfile.indexOf("</")));
-				textfile = textfile.substring(textfile.indexOf(CELL)+CELL.length());
-				
-				System.out.println(matriz.get(matriz.size()-1));
-				
+				//cuando no hay mas coincidencias tira la excepcion
+				colheads.substring(colheads.indexOf(KEYWRD));
+	
+				colheads = colheads.substring(colheads.indexOf(KEYWRD) + KEYWRD.length());
+			    
 			} catch(IndexOutOfBoundsException e) {
 				break;
 			}
 			
+			columnas++;
 		}
 		
+		//System.out.printf("\nCantidad de columnas = %d\n\n", columnas);
 		
-	}
-	
-	public void sensibilizadas(){
+		Vector<int[]> matriz = new Vector<int[]>();
 		
-	}
-	
-	public boolean disparar(){
 		
-		return false;
+		while(true) {
+			
+			int[] col = new int[columnas];
+			
+			for(int i=0; i<columnas; i++) {
+				
+				try {
+					
+					//cuando no hay mas coincidencias tira la excepcion
+					textfile = textfile.substring(textfile.indexOf(CELL));
+					
+					String number = textfile.substring(CELL.length(), textfile.indexOf("</")).trim();
+					col[i] = Integer.parseInt(number);
+					
+					textfile = textfile.substring(textfile.indexOf(CELL)+CELL.length());
+					
+				} catch(IndexOutOfBoundsException e) {
+					matriz.add(col);
+					return matriz;
+				}
+				
+			}
+			
+			matriz.add(col);
+			
+		}
+		
 	}
 	
 }
